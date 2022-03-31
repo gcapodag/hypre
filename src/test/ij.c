9a1862fa67462e6674170eeba8161a841faf679f
gcapodag <gcapodaglio@lanl.gov>
2022-03-31 10:36:26 -0600

    changed tol in ij.c

--- src/test/ij.c
+++ src/test/ij.c
@@ -268,7 +268,7 @@ main( hypre_int argc,
    HYPRE_Real   relax_wt_level;
    HYPRE_Real   outer_wt;
    HYPRE_Real   outer_wt_level;
-   HYPRE_Real   tol = 1.e-8, pc_tol = 0.;
+   HYPRE_Real   tol = 1.e-11, pc_tol = 0.;
    HYPRE_Real   atol = 0.0;
    HYPRE_Real   max_row_sum = 1.;
    HYPRE_Int    converge_type = 0;
@@ -3970,13 +3970,14 @@ main( hypre_int argc,
 
       if (solver_id == 0)
       {
+         //HYPRE_ParVectorPrint(x, "solution.out");
          HYPRE_BoomerAMGDestroy(amg_solver);
       }
       else if (solver_id == 90)
       {
          HYPRE_BoomerAMGDDDestroy(amgdd_solver);
       }
-    } 
+    }
 
     //} //END of iC loop 
 
